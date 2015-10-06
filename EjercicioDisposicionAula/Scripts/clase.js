3b550a3aa2fd65de5789e44066f492b33ef1cc11
luis <luis.gil@tajamar365.com>
2015-10-06 10:05:42 +0200

    final

---
+++ EjercicioDisposicionAula/Scripts/clase.js
@@ -0,0 +1,66 @@
+﻿var disposicion = [];
+var url = "https://alumnoscurso.azure-mobile.net/Tables/Clase";
+function dibujarCanvas(pos) {
+    var canvas = document.getElementById("clase");
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle=pos.color;
+    ctx.fillRect(pos.x, pos.y, pos.w, pos.h);
+}
+function recuperar() {
+    if (localStorage["elementos"]) {
+        disposicion = JSON.parse(localStorage["elementos"]);
+        for (var i = 0; i < disposicion.length; i++) {
+            dibujarCanvas(disposicion[i]);
+        }
+    }
+}
+
+function guardar() {
+    var obj= {
+        x: parseInt(document.getElementById("txtX").value),
+        y: parseInt(document.getElementById("txtY").value),
+        w: parseInt(document.getElementById("txtW").value),
+        h: parseInt(document.getElementById("txtH").value),
+        color: document.getElementById("txtCol").value,
+        nombre: localStorage["nombre"]
+    }
+    disposicion.push(obj);
+    dibujarCanvas(obj);
+    $.ajax({
+        url: url,
+        method: "POST",
+        data: JSON.stringify(obj),
+        dataType: "json",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        success: function() {
+            alert("Guardado");
+        },
+        error: function() {
+            alert("Fallo");
+        }
+    });
+    localStorage["elementos"] = JSON.stringify(disposicion);
+}
+function refrescar() {
+    var filtro = "?$filter=nombre eq '" + localStorage["nombre"] +
+        "'";
+    $.get(url+filtro, null, function(res) {
+
+        for (var i = 0; i < res.length; i++) {
+            dibujarCanvas(res[i]);
+        }
+    });
+
+
+}
+
+if (!localStorage["nombre"])
+    location.replace("index.html");
+
+
+document.getElementById("btnOk").addEventListener("click", guardar);
+document.getElementById("btnRefrescar").
+    addEventListener("click", refrescar);
+recuperar();
---
+++ EjercicioDisposicionAula/Scripts/index.js
@@ -0,0 +1,17 @@
+﻿if (localStorage.getItem("nombre")) {
+    location.href = "clase.html";
+}
+
+
+function guardar() {
+    var ele = document.getElementById("txtNom").value;
+    if (ele == "") {
+        alert("El nombre no puede estar vacio");
+        return;
+    }
+    localStorage.setItem("nombre",ele);
+    location.href = "clase.html";
+}
+
+
+document.getElementById("btnOk").addEventListener("click",guardar);
